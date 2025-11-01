54b86709bb39057d6588294c949736f9a44f0a30
chsami <sami.chkhachkhi@gmail.com>
2025-11-01 21:47:26 +0100

    chore: fix basalt mining AutoMiningPlugin and AutoMiningScript

--- src/main/java/net/runelite/client/plugins/microbot/mining/AutoMiningPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/mining/AutoMiningPlugin.java
@@ -24,7 +24,7 @@ import java.awt.*;
 )
 @Slf4j
 public class AutoMiningPlugin extends Plugin {
-    public static final String version = "1.0.8";
+    public static final String version = "1.0.9";
     @Inject
     private AutoMiningConfig config;
     @Provides
